be63d00b970df9365ad24e91df7508c576d6d986
kartone-s1 <mario.ciccarelli@sentinelone.com>
2024-09-03 10:39:55 +0000

    First commit

---
+++ ad/GOAD-Light/files/srv01/install_s1.ps1
@@ -0,0 +1,5 @@
+$package = "SentinelOneInstaller_windows_64bit_v23_4_4_223.exe"
+$siteToken = "eyJ1cmwiOiAiaHR0cHM6Ly91c2VhMS1wdXJwbGUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogIjdkMjVlOWJjOWNmYmZkMmQifQ=="
+
+# Install the SentinelOne Agent
+Start-Process -FilePath $package -ArgumentList "-t $siteToken -q" -Wait
