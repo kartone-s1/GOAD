9ba34d388413c23aaa334daf0f64a7af4472a3e6
Mario Ciccarelli <152268925+kartone-s1@users.noreply.github.com>
2024-09-12 11:13:07 +0200

    Update install_s1.ps1
    Added a disclaimer note for SentinelOne agent site token registration.

--- ansible/roles/settings/sentinelone/files/install_s1.ps1
+++ ansible/roles/settings/sentinelone/files/install_s1.ps1
@@ -1,4 +1,5 @@
 $package = "c:\tmp\SentinelOneInstaller_windows_64bit_v23_4_4_223.exe"
+# This token belongs to my site on Purple. Change it accordingly, or your machines will register in my site!
 $siteToken = "eyJ1cmwiOiAiaHR0cHM6Ly91c2VhMS1wdXJwbGUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogIjdkMjVlOWJjOWNmYmZkMmQifQ=="
 
 # Install the SentinelOne Agent
